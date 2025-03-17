e03304a098e63a93face8dbcf18f27a8eda719c9
udud0099 <udud0099@gmail.com>
2025-03-17 15:19:02 +0545

    js restart

--- MY Project/-1 Math for Kids Application/script.js
+++ MY Project/-1 Math for Kids Application/script.js
@@ -54,7 +54,6 @@ function opration() {
 }
 function finalShowHtml() {
   document.getElementById("que").innerHTML = firVal + oprHtml + secVal;
-  document.getElementById("opt1").innerHTML = ans;
   orderOfAns();
 }
 function orderOfAns() {
@@ -65,20 +64,20 @@ function orderOfAns() {
       a.push(aRan);
     }
   }
+  wrongAns();
   document.getElementById("opt1").style.order = a[0];
   document.getElementById("opt2").style.order = a[1];
   document.getElementById("opt3").style.order = a[2];
   document.getElementById("opt4").style.order = a[3];
-  wrongAns();
 }
 
 function wrongAns() {
   let wrongAnsRandom = ans;
-  console.log("ans" + ans);
+  console.log("ans " + ans);
 
   if (wrongAnsRandom < 5) {
     if (wrongAnsRandom > -5) {
-      wrongAnsRandom = 6;
+      wrongAnsRandom = 7;
     }
   }
   let Wopt2 = Math.floor(Math.random() * wrongAnsRandom);
@@ -86,14 +85,22 @@ function wrongAns() {
   let Wopt4 = Math.floor(Math.random() * wrongAnsRandom);
   console.log("one " + ans, Wopt2, Wopt3, Wopt4);
 
-  checkWrongAns(Wopt2, Wopt3, Wopt4);
+  checkWrongAns(ans, Wopt2, Wopt3, Wopt4);
 }
-function checkWrongAns(Wopt2, Wopt3, Wopt4) {
+function checkWrongAns(ans, Wopt2, Wopt3, Wopt4) {
   console.log("two " + ans, Wopt2, Wopt3, Wopt4);
 
-  if (Wopt2 == Wopt3 || Wopt2 == Wopt4 || Wopt3 == Wopt4) {
+  if (
+    ans === Wopt2 ||
+    ans === Wopt3 ||
+    ans === Wopt4 ||
+    Wopt2 === Wopt3 ||
+    Wopt2 === Wopt4 ||
+    Wopt3 === Wopt4
+  ) {
     wrongAns();
   } else {
+    document.getElementById("opt1").innerHTML = ans;
     document.getElementById("opt2").innerHTML = Wopt2;
     document.getElementById("opt3").innerHTML = Wopt3;
     document.getElementById("opt4").innerHTML = Wopt4;
