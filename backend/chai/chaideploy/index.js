e7066f59bf0f7aefa3b184dca5dfff96f3c0c92a
udud0099 <udud0099@gmail.com>
2025-03-23 12:03:24 +0545

    js backend with chai - https://www.youtube.com/playlist?list=PLu71SKxNbfoBGh_8p_NS-ZAh6v7HhYqHW - start

---
+++ backend/chai/chaideploy/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("twitter page");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>login first</h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>who am i</h2>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
