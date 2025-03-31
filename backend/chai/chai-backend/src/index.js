83f49afbaa90b8d47b144f4becb71b83e598aa56
udud0099 <udud0099@gmail.com>
2025-03-31 12:58:05 +0545

    v8 - backend chai chai-backend - How to connect database in MERN with debugging

---
+++ backend/chai/chai-backend/src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// import express from "express";
+// const app = express()(async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//     app.on("error", (error) => {
+//       console.log("ERRR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`app is ready to lunch ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Error", error);
+
+//     throw err;
+//   }
+// })();
